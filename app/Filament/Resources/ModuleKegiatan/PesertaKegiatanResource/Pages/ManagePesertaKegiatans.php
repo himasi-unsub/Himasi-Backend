ea230aac6991f558da66e40261554380b2f5ad51
ryzntx <thalalatha13@gmail.com>
2025-09-10 15:08:06 +0700

    feat: Add Excel import functionality to ManagePesertaKegiatans page

--- app/Filament/Resources/ModuleKegiatan/PesertaKegiatanResource/Pages/ManagePesertaKegiatans.php
+++ app/Filament/Resources/ModuleKegiatan/PesertaKegiatanResource/Pages/ManagePesertaKegiatans.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ModuleKegiatan\PesertaKegiatanResource\Pages;
 
 use App\Filament\Resources\ModuleKegiatan\PesertaKegiatanResource;
 use Filament\Actions;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManagePesertaKegiatans extends ManageRecords
@@ -13,6 +14,19 @@ class ManagePesertaKegiatans extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
+            \EightyNine\ExcelImport\ExcelImportAction::make('importExcel')
+                ->label('Import Excel')
+                ->sampleExcel(
+                    sampleData: [
+                        'id_kegiatan_acara'           => '1',
+                        'id_mahasiswa'         => '1',
+                    ],
+                    fileName: 'peserta-kegiatan-example-data.xlsx',
+                    sampleButtonLabel: 'Download Sample',
+                    customiseActionUsing: fn(Action $action) => $action->color('secondary')
+                        ->icon('heroicon-m-clipboard')
+                        ->requiresConfirmation(),
+                ),
             Actions\CreateAction::make(),
         ];
     }
